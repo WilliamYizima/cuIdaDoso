2cf8d1e72445dc9d5429c615f5d3315a24b4d1fc
Will <william.yizima@hotmail.com>
2019-08-15 18:19:34 -0300

    Comit teste

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function (req, res) {
+	console.log("Port Number : 5000");
+	// change the MIME type from 'text/plain' to 'text/html'
+    res.writeHead(200, {'Content-Type': 'text/html'});
+	//reading the content file
+	fs.readFile('horas.html', (err, data) => {
+		//checking for errors
+		if (err) 
+			throw err;
+		console.log("Operation Success");
+		//sending the response
+		res.end(data);
+	});
+}).listen(5000);
+					
