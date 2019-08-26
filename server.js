8bd4ce554ce8cdc387521cf33ddb80db42b5b14b
Will <39131639+WilliamYizima@users.noreply.github.com>
2019-08-26 00:30:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();         
-const bodyParser = require('body-parser');
+// const express = require('express');
+const app = require('./src/config/custom-express');	
+// const app = express();         
+// const bodyParser = require('body-parser');
 const port = 3000; //Porta padrão
 const mysql = require('mysql');
 
